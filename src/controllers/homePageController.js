210381c26132db57bdd7722390041b3ca39c9897
Yohan nouet <yohan.nouet@gmail.com>
2021-02-08 20:11:37 +0100

    first param provided to the homepage

--- src/controllers/homePageController.js
+++ src/controllers/homePageController.js
@@ -1,7 +1,30 @@
+import OTPService from "../services/OTPService";
+
 let handleHelloWorld = async (req, res) => {
-    return res.render("homepage.ejs",{
-        user: req.user
-    });
+    try {
+        OTPService.getAllSMS()
+            .then((data)=>{
+                //console.log(data);
+                return res.render("homepage.ejs",{
+                    messages: data
+                });
+            })
+            .catch(err => {
+                console.log(err);
+                return res.render("homepage.ejs",{
+                    messages: null
+                });
+            });
+
+
+
+    } catch (err) {
+        req.flash("errors", err);
+        return res.render("homepage.ejs",{
+            messages: null
+        });
+    }
+
 };
 
 module.exports = {
